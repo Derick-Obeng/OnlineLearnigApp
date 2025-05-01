5e69aa687cc5a098af3433ce44d6f5cfd7242d4d
BeatriceAddai <beatriceaddai59@gmail.com>
2025-05-01 16:25:26 +0000

    Course Filter Screen

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import RootStack from './tabs/tabs';
+import CourseFilterScreen from './screens/CourseFilterScreen';
 
 
 
@@ -11,8 +12,9 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-       <RootStack/>
+      < CourseFilterScreen />
     </NavigationContainer>
+    
   );
 }
 
